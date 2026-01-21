bf7d911c1227713bd9bf366b690d8fd18cd3c904
Jess Friedersdorff <jess.friedersdorff@ukhsa.gov.uk>
2026-01-21 12:20:24 +0000

    fix: fixed string formatting for logs.

--- src/claspar/bacteria.py
+++ src/claspar/bacteria.py
@@ -332,10 +332,11 @@ class SylphBacteria:
         Get the species taxon ID and the species name from row, using taxaplease.
 
         :params row: pd.series, row of a dataframe (used with an apply).
-        :return: list of two; taxon ID and human-readable species name, or [None, None] if rank is not species or strain.
+        :return: list of two; taxon ID and human-readable species name, or [None, None] if rank is not species or
+        strain.
         """
         if row["taxon_id"] is None:
-            logging.error(f"Taxon id is not found for {row}")
+            logging.error("Taxon id is not found for %s" % (row))  # noqa
             return None, None
 
         if row["taxon_rank"] == "species":
@@ -349,7 +350,9 @@ class SylphBacteria:
             else:
                 return None, None
         else:
-            print(f"Taxon ID returned rank other than species or strain: {row['taxon_id'], row['taxon_rank']}")
+            logging.info(
+                "Taxon ID returned rank other than species or strain: %s, %s" % (row["taxon_id"], row["taxon_rank"])  # noqa
+            )
             return None, None
 
     def _get_sylph_confidence_rating(
